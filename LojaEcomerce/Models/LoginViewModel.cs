c2b9100a2baf838bd6792fef31dfd64a160e9755
Wellington <welltecnic@hotmail.com>
2026-05-06 17:23:50 -0300

    refactor: correção model loginview

--- LojaEcomerce/Models/LoginViewModel.cs
+++ LojaEcomerce/Models/LoginViewModel.cs
@@ -3,10 +3,13 @@
     public class LoginViewModel
     {
         public int Id { get; set; }
-        public string? Nome { get; set; } 
-        public string? Email { get; set; }
-        public string? Senha { get; set; }
-        public string? Nivel { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Senha { get; set; } = string.Empty;
+        public string Nivel { get; set; } = "Usuario";
 
     }
+  
+
+
 }
