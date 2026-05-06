e3f92444b9d0e3c6917d4bf4ff6e7be39581c898
Wellington <welltecnic@hotmail.com>
2026-05-06 17:24:41 -0300

    fix:corringindo injeção de dependencia

--- LojaEcomerce/Program.cs
+++ LojaEcomerce/Program.cs
@@ -1,14 +1,18 @@
 using LojaEcomerce.Interfaces;
 using LojaEcomerce.Repositorio;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Usuario/Logar";
+        options.AccessDeniedPath = "/Usuario/AcessoNegado";
+    });
+
 // INJEÇÃO DE DEPENDENCIA
 builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
-
-
-
-
 
 
 
@@ -26,6 +30,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+//MUITO IMPORTANTE COLOCAR
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
